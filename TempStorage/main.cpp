69daac68e3447fb4ce67b1ba028e8d973c9c47a3
Michael Knaup <Michael.Knaup@me.com>
2013-09-19 22:43:47 +0200

    Initial Commit

---
+++ TempStorage/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TempStorage
+//
+//  Created by Michael Knaup on 19.09.13.
+//  Copyright (c) 2013 Michael Knaup. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
